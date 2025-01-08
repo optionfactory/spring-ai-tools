34cf5532f19424a11b019fbc892fa81fa6ba145b
Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
2025-01-08 10:25:43 +0100

    :sparkles: add truncating text based on tokens

---
+++ src/main/java/net/optionfactory/springai/Neuralyzer.java
@@ -0,0 +1,46 @@
+package net.optionfactory.springai;
+
+import com.knuddels.jtokkit.Encodings;
+import com.knuddels.jtokkit.api.ModelType;
+
+/**
+ * See: <a href="https://meninblack.fandom.com/wiki/Neuralyzer">explanation of the name</a>
+ */
+public class Neuralyzer {
+
+    /**
+     * Forget the final part of the text counting token with GPT_4O model.
+     *
+     * @param text      input text
+     * @param maxTokens max tokens to shrink the output
+     * @return truncated text
+     */
+    public static String forget(String text, int maxTokens) {
+        return forget(text, maxTokens, ModelType.GPT_4O);
+    }
+
+    /**
+     * Forget the final part of the text counting token with specified model.
+     *
+     * @param text      input text
+     * @param maxTokens max tokens to shrink the output
+     * @param modelType specify the model type to use for token count
+     * @return truncated text
+     */
+    public static String forget(String text, int maxTokens, ModelType modelType) {
+        final var registry = Encodings.newDefaultEncodingRegistry();
+        final var encoding = registry.getEncodingForModel(modelType);
+        var currentString = text;
+        while (encoding.countTokens(currentString) > maxTokens) {
+            float avgTokenLength = (float) currentString.length() / encoding.countTokens(currentString);
+            float maxTokensLength = avgTokenLength * maxTokens;
+            if (maxTokensLength >= currentString.length()) {
+                throw new IllegalStateException("Could not shorten string when truncating");
+            }
+            currentString = currentString.substring(0, (int) maxTokensLength);
+        }
+        return currentString;
+    }
+
+
+}
---
+++ src/test/java/net/optionfactory/springai/NeuralyzerTest.java
@@ -0,0 +1,25 @@
+package net.optionfactory.springai;
+
+import com.knuddels.jtokkit.api.ModelType;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class NeuralyzerTest {
+
+
+    @Test
+    public void testTruncation() {
+        final var loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        final var truncatedLoremIpsum = Neuralyzer.forget(loremIpsum, 50);
+        assertTrue(truncatedLoremIpsum.length() < loremIpsum.length());
+    }
+
+    @Test
+    public void testTruncationWithDifferentModel() {
+        final var loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        final var truncatedLoremIpsum = Neuralyzer.forget(loremIpsum, 50, ModelType.GPT_4);
+        assertTrue(truncatedLoremIpsum.length() < loremIpsum.length());
+    }
+
+}
