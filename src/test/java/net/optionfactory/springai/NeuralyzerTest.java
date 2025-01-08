1f01b1331476ea65a120508ad46976a7b5413abe
Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
2025-01-08 10:40:59 +0100

    :white_check_mark: add simple test

--- src/test/java/net/optionfactory/springai/NeuralyzerTest.java
+++ src/test/java/net/optionfactory/springai/NeuralyzerTest.java
@@ -3,6 +3,7 @@ package net.optionfactory.springai;
 import com.knuddels.jtokkit.api.ModelType;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class NeuralyzerTest {
@@ -20,6 +21,13 @@ class NeuralyzerTest {
         final var loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
         final var truncatedLoremIpsum = Neuralyzer.forget(loremIpsum, 50, ModelType.GPT_4);
         assertTrue(truncatedLoremIpsum.length() < loremIpsum.length());
+    }
+
+    @Test
+    public void doNotTruncateIfAlreadyUnderLimit() {
+        final var loremIpsum = "Lorem ipsum dolor sit amet, consectetur adipisci elit, sed do eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullamco laboriosam, nisi ut aliquid ex ea commodi consequatur. Duis aute irure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        final var truncatedLoremIpsum = Neuralyzer.forget(loremIpsum, 5000);
+        assertEquals(loremIpsum, truncatedLoremIpsum);
     }
 
 }
