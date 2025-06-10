b05effb83192154e102602462a2786eff3dd7a9e
Lorenzo Caenazzo <lorenzo.caenazzo@optionfactory.net>
2025-06-10 17:06:07 +0200

    :sparkles: add enqueued and dequeued meters (counters)

--- src/main/java/net/optionfactory/springai/monitoring/QueueMeter.java
+++ src/main/java/net/optionfactory/springai/monitoring/QueueMeter.java
@@ -1,5 +1,6 @@
 package net.optionfactory.springai.monitoring;
 
+import io.micrometer.core.instrument.Counter;
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 
@@ -8,8 +9,24 @@ import java.util.function.Supplier;
 import static net.optionfactory.springai.monitoring.MetricNameProvider.getMetricName;
 
 public class QueueMeter {
+    private final Counter enqueuedCounter;
+    private final Counter dequeuedCounter;
+
     public QueueMeter(MeterRegistry meterRegistry, Supplier<Number> queueLengthSupplier) {
         Gauge.builder(getMetricName("queue.length"), queueLengthSupplier)
                 .register(meterRegistry);
+
+        this.enqueuedCounter = Counter.builder(getMetricName("queue.enqueued"))
+                .register(meterRegistry);
+        this.dequeuedCounter = Counter.builder(getMetricName("queue.dequeued"))
+                .register(meterRegistry);
+    }
+
+    public void enqueued() {
+        enqueuedCounter.increment();
+    }
+
+    public void dequeued() {
+        dequeuedCounter.increment();
     }
 }
